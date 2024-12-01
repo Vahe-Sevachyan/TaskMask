92c4c44125a654464879258361f9ef1b24f4a618
Vahe Sevachyan <vscode13@gmail.com>
2024-11-30 16:28:29 -0800

    updated bulma ui

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-const input = document.createElement(".input");
-const textArea = document.createElement(".textarea");
+const taskDB = [];
+const ctBtn = document.querySelector(".ct-btn");
+function CreateTask(taskName, taskInfo) {
+  this.taskName = taskName;
+  this.taskInfo = taskInfo;
+}
 
-const tasks = {};
+ctBtn.addEventListener("click", () => {
+  const input = document.querySelector(".input").value;
+  const textArea = document.querySelector(".textarea").value;
+  const newTask = new CreateTask(input, textArea);
+  taskDB.push(newTask);
+  console.log(taskDB);
+  console.log("clicked");
+});
